535e7966e243f16f6e424f6155b20a6110dfa4b3
Social Codia <socialcodia@gmail.com>
2022-02-12 03:10:33 +0530

    Bug Fixes

--- dtos/mcq-dto.js
+++ dtos/mcq-dto.js
@@ -14,8 +14,9 @@ class McqDto {
         this.question = data.question;
         this.opOne = data.opOne;
         this.opTwo = data.opTwo;
+        this.opThree = data.opThree;
         this.opFour = data.opFour;
-        this.answer = data.answer==1? data.opOne : data.answer ==2 ? data.opTwo : data.answer ==3 ? data.opThree : data.answer ==4 ? data.opFour : data.answer;
+        this.answer = data.answer == 1 ? data.opOne : data.answer == 2 ? data.opTwo : data.answer == 3 ? data.opThree : data.answer == 4 ? data.opFour : data.answer;
     }
 
 }
