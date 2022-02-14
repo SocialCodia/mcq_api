ac9e4c6a6d29f8bf29859131c27ea3c131ea6f13
Social Codia <socialcodia@gmail.com>
2022-02-14 11:36:06 +0530

    Update

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -33,11 +33,11 @@ class AuthController {
         }
         const { accessToken } = tokenService.generateToken(payload);
         res.cookie('accessToken', accessToken, {
-            maxAge: 1000 * 60 * 60 * 24 * 30
+            maxAge: 1000 * 60 * 60 * 24 * 30,
         });
-
+        res.setHeader('SameSite', 'None');
         const data = new UserDto(user);
-        data.accessToken = accessToken; 
+        data.accessToken = accessToken;
         res.json({ success: true, message: 'Login Successfull', data });
     }
 }
