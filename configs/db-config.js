f564aecc136523b80c4da968b2f2b7adc514e6db
Social Codia <socialcodia@gmail.com>
2022-02-14 11:19:35 +0530

    Update DB_URL from Local to Online

--- configs/db-config.js
+++ configs/db-config.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const dbConnection = () => {
 
-    // const dbUrl = process.env.DB_URL || 'mongodb+srv://socialcodia:uZqEPCgjU4RsHkYR@cluster0.gkvfj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-    const dbUrl = 'mongodb://localhost:27017/mcq'
+    const dbUrl = process.env.DB_URL || 'mongodb+srv://socialcodia:uZqEPCgjU4RsHkYR@cluster0.gkvfj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    // const dbUrl = 'mongodb://localhost:27017/mcq'
     mongoose.connect(dbUrl).then(() => console.log('Database Connected'))
         .catch(err => console.log(`Failed To Connect Database, Reason : ${err}`))
 
