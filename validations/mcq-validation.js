520ac1cd367a94a7ef91a31a97733a38c91d9c6d
Social Codia <socialcodia@gmail.com>
2022-02-16 15:27:32 +0530

    Bug Fixes

--- validations/mcq-validation.js
+++ validations/mcq-validation.js
@@ -4,10 +4,10 @@ class McqValidation {
 
     createMcq = Joi.object({
         question: Joi.string().min(2).required(),
-        opOne: Joi.string().optional(),
-        opTwo: Joi.string().optional(),
-        opThree: Joi.string().optional(),
-        opFour: Joi.string().optional(),
+        opOne: Joi.string().optional().default(''),
+        opTwo: Joi.string().optional().default(''),
+        opThree: Joi.string().optional().default(''),
+        opFour: Joi.string().optional().default(''),
         answer: Joi.number().min(1).max(4).default(1),
         verified: Joi.bool().default(true)
     });
